a471d51a36c90dbebb4ff0f35d0c427dd1d6a6db
Shakir <shakir-redhead@outlook.com>
2026-02-05 11:51:08 +0000

    chore(sops): add laptop age key and update secrets

--- nix/modules/users/default.nix
+++ nix/modules/users/default.nix
@@ -12,6 +12,7 @@ in {
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMgak38Af+nKukrQHPyqtqrl6VI/vEhivai6+0KrASTV shakir@homelab"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFI5k1s3bXew4ud5tt9uI01/zZrTHilqSrayU8C9J0IQ shakir@homelab-laptop"
     ];
   };
 }
